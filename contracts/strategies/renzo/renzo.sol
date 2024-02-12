adc317da56182a85e57e69cdd90642c840342695
“TeddyNotBear” <notbearteddy@gmail.com>
2024-02-12 14:36:13 +0100

    feat: init renzo strategy

---
+++ contracts/strategies/renzo/renzo.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.20;
+
+import {StrategyBase} from "../StrategyBase.sol";
+import "../../interfaces/IStETH.sol";
+
+contract RenzoStrategy is StrategyBase {
+    IStETH public stETH;
+
+    function initialize(
+        address _poolingManager,
+        address _underlyingToken, // ETH Address
+        address _yieldToken // StETH address
+    ) public virtual initializer {
+        initializeStrategyBase(_poolingManager, _underlyingToken, _yieldToken);
+    }
+
+    // On Starknet : fn initiate_withdraw(ref self: ContractState, l1_recipient: felt252, amount: u256);
+    function _deposit(uint256 amount) internal override {
+        require(address(this).balance != 0, "ZERO_DEPOSIT");
+        // Submit to LIDO
+        // https://docs.lido.fi/contracts/lido#submit-1
+        IStETH(STETH).submit(address(0));
+        // Call RestakeManager.deposit(IERC20 _collateralToken, uint256 _amount, uint256 _referralId)
+        // _collateralToken: stETH address
+        // The msg.sender must pre-approve this contract to move the tokens into the protocol
+        // https://github.com/Renzo-Protocol/contracts-public/blob/master/contracts/RestakeManager.sol
+    }
+
+    function _withdraw(uint256 amount) internal override returns (uint256) {}
+    
+}
